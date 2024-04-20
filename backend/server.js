098764d1cbb5bbc8aba97d8d3514a297ba000a90
Param Shankar <paramshankar@Params-MacBook-Pro.local>
2024-04-20 12:41:11 +0530

    new file

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+
+const app = express()
+app.listen(3000, () => {
+    console.log("listing to port 3000");
+})
+
+app.get('/', (req,res) => {
+    res.send("hello world");
+})
